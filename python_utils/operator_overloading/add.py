1ad70df9f15d2b5a505a925b82681567261d4f97
RaviG <39865757+ravigurnatham@users.noreply.github.com>
2024-06-23 12:41:25 -0500

    adding a directory dedicated for operator overloading and started with __add__

---
+++ python_utils/operator_overloading/add.py
@@ -0,0 +1,17 @@
+class Point:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+  def __add__(self, other):
+    if isinstance(other, Point):
+      return Point(self.x + other.x, self.y + other.y)
+    else:
+      raise TypeError("Invalid operand type for +: 'Point' and '{}'".format(type(other)))
+
+p1 = Point(1, 2)
+p2 = Point(3, 4)
+
+p3 = p1 + p2
+
+print(p3.x, p3.y)
